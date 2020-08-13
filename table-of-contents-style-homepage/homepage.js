9326bb45e2cf8fb700e16fe16bbea508f2d118c8
Arthur Smid <smidarthur@gmail.com>
2020-08-12 22:02:52 -0700

    experiment with creating a pop-up div for the find help button on the mobile intro screen

--- table-of-contents-style-homepage/homepage.js
+++ table-of-contents-style-homepage/homepage.js
@@ -1,11 +1,10 @@
-
 //  The popup description appears when someone clicks the blue crisis text 
-
 function crisisPopup() {
     var popup = document.getElementById("myPopup");
     popup.classList.toggle("show");
   }
 
+  
 /* When the user clicks on the button, toggle between hiding and showing the dropdown content */
 function cityMenu() {
   document.getElementById("myDropdown").classList.toggle("show");
@@ -30,3 +29,8 @@ function emergencyCrisis() {
 }
 
 
+// This function is for the mobile intro-screens, clicking on the Find Help button displays this page within index.html
+function findHelp() {
+  var help = document.getElementById("helpButton");
+  help.classList.toggle("show");
+}
